cc2d55a9754df3a73f672b8786e97c391234a6c8
kiennmhe171466 <144405697+kiennmhe171466@users.noreply.github.com>
2023-09-21 14:43:51 +0700

    Update DBContext.java

--- QuizThink/src/java/DAL/DBContext.java
+++ QuizThink/src/java/DAL/DBContext.java
@@ -1,27 +1,44 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package DAL;
+package Dal;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
- * @author LEMONLORD
+ * @author User
  */
 public class DBContext {
+    
+    protected Connection connection;
 
-    public Connection getConnection() throws Exception {
-        String url = "jdbc:sqlserver:// " + serverName + ":" + portNumber + ";databaseName=" + dbName;
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        return DriverManager.getConnection(url, userID, password);
+    public DBContext() {
+        try {
+            String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+            if (instance == null || instance.trim().isEmpty()) {
+                url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+            }
+            String username = "sa";
+            String password = "admin";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, userID, password);
+            System.out.println("===== CONNECTED DB: " + connection + "=====\n");
+        } catch (ClassNotFoundException | SQLException ex) {
+        }
     }
+
     private final String serverName = "localhost";
-    private final String dbName = "SWP391_QuizPractice_QuizThink";
+    private final String dbName = "SWP391_QuizPractice_QuizThink"; //Database name is HERE  <==
     private final String portNumber = "1433";
+    private final String instance = "";
     private final String userID = "sa";
     private final String password = "admin";
-
+    
+    public static void main(String[] args) {
+        DBContext db = new DBContext();
+    }
 }
